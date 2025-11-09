59b500fbec9eb09addf34ecdb8d342ee62173258
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2025-11-09 11:11:42 -0800

    Comment the test harness

--- fpp_analysis/src/test/lib.rs
+++ fpp_analysis/src/test/lib.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use std::{env, fs};
 
 pub(crate) fn run_test(file_path: &str) {
+    // Compute the path to the FPP input and .ref.txt output
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.push("src/test");
 
@@ -15,9 +16,12 @@ pub(crate) fn run_test(file_path: &str) {
     ref_file.push(file_path);
     ref_file.set_extension("ref.txt");
 
+    // Set up the compiler context to capture diagnostic messages into a buffer
     let mut diagnostics_str = vec![];
     let mut ctx =
         fpp_core::CompilerContext::new(fpp_errors::WriteEmitter::new(&mut diagnostics_str));
+
+    // Parse the input and run the semantic checker on the AST
     fpp_core::run(&mut ctx, || {
         let source_file_path = fpp_file.to_str().unwrap();
         let src = match SourceFile::open(source_file_path) {
@@ -35,6 +39,7 @@ pub(crate) fn run_test(file_path: &str) {
         .expect("failed to convert error message to string")
         .replace(path.to_str().unwrap(), "[ local path prefix ]");
 
+    // Validate the diagnostic messages against the reference file
     match env::var("FPP_UPDATE_REF") {
         Ok(_) => {
             // Update the ref file
